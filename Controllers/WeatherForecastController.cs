6b16462f76d958c77f1d1a2af8b965811fb76803
Cristiano Motta <cristiano.motta@hotmail.it>
2020-05-27 10:42:24 +0200

    First Commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace RedisStudio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IDistributedCache DistributedCache;
+        private readonly RedisCacheHelper RedisCache;
+
+        public WeatherForecastController(IDistributedCache distributedCache, RedisCacheHelper redisCache)
+        {
+            DistributedCache = distributedCache;
+            RedisCache = redisCache;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var cacheKey = "TheTime";
+            var existingTime = DistributedCache.GetString(cacheKey);
+            if (!string.IsNullOrEmpty(existingTime))
+            {
+                return "Fetched from cache : " + existingTime;
+            }
+            else
+            {
+                existingTime = DateTime.UtcNow.ToString();
+                DistributedCache.SetString(cacheKey, existingTime);
+                return "Added to cache : " + existingTime;
+            }
+        }
+
+        [Route("list")]
+        [HttpGet]
+        public List<string> GetList()
+        {
+            var cacheKey = "list";
+
+            var lista = RedisCache.Get<List<string>>(cacheKey);
+            if (lista != null)
+            {
+                return lista;
+            }
+            else
+            {
+                RedisCache.Set(cacheKey, new List<string>{"pippo", "pluto", "paperino"});
+                return RedisCache.Get<List<string>>(cacheKey);
+            }
+        }
+    }
+}
---
+++ RedisCacheHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RedisStudio
+{
+    public class RedisCacheHelper
+    {
+        private readonly IDistributedCache DistributedCache;
+
+        public RedisCacheHelper(IDistributedCache distributedCache)
+        {
+            DistributedCache = distributedCache;
+        }
+
+        public T Get<T>(string cacheKey)
+        {
+            return Deserialize<T>(DistributedCache.Get(cacheKey));
+        }
+
+        public void Set(string cacheKey, object cacheValue)
+        {
+            DistributedCache.Set(cacheKey, Serialize(cacheValue));
+        }
+
+        public static byte[] Serialize(object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            BinaryFormatter objBinaryFormatter = new BinaryFormatter();
+            using (MemoryStream objMemoryStream = new MemoryStream())
+            {
+                objBinaryFormatter.Serialize(objMemoryStream, obj);
+                byte[] objDataAsByte = objMemoryStream.ToArray();
+                return objDataAsByte;
+            }
+        }
+
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            BinaryFormatter objBinaryFormatter = new BinaryFormatter();
+            if (bytes == null)
+                return default(T);
+
+            using (MemoryStream objMemoryStream = new MemoryStream(bytes))
+            {
+                T result = (T) objBinaryFormatter.Deserialize(objMemoryStream);
+                return result;
+            }
+        }
+    }
+}
