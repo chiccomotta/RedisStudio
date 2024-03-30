c806af11364f56d4822726a86a23016bada53584
Cristiano Motta <cristiano.motta@hotmail.it>
2024-03-30 15:13:58 +0100

    Minor changes

--- Controllers/Multiplexer.cs
+++ Controllers/Multiplexer.cs
@@ -1,104 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using RedisStudio.DbContext;
 using StackExchange.Redis;
 using System.Linq;
 using System.Threading.Tasks;
 using CM.RedisCache;
+using RedisStudio.Utility;
 
-namespace RedisStudio.Controllers
+namespace RedisStudio.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class Multiplexer : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class Multiplexer : ControllerBase
+    private readonly IConnectionMultiplexer multiplexer;
+    private readonly IDatabase redisDatabase;
+    private readonly MyContext _context;
+
+    public Multiplexer(IConnectionMultiplexer _multiplexer, IDatabase _redisDatabase, MyContext context)
     {
-        private readonly IConnectionMultiplexer multiplexer;
-        private readonly IDatabase redisDatabase;
-        private readonly MyContext _context;
+        this.multiplexer = _multiplexer;
+        this.redisDatabase = _redisDatabase;
+        _context = context;
+    }
 
-        public Multiplexer(IConnectionMultiplexer _multiplexer, IDatabase _redisDatabase, MyContext context)
+    [Route("feed")]
+    [HttpGet]
+    public IActionResult Feed()
+    {
+        List<Travel> travels = DbUtility.Feed(100);
+
+        _context.Travel.AddRange(travels);
+        _context.SaveChanges();
+
+        return Ok("Feed OK");
+    }
+
+    [Route("db")]
+    [HttpGet]
+    public IActionResult Db()
+    {
+        var query =  _context.Travel.Where(i => i.Enabled == true && i.City.StartsWith("Port"));
+        //var key = query.GetCacheKey();
+
+        var result = query.FromCache(TimeSpan.FromHours(24));
+
+        return Ok(result);
+    }
+
+    [Route("write")]
+    [HttpGet]
+    public async Task<IActionResult> Write()
+    {
+        await redisDatabase.StringSetAsync("key3", "Valore 3.1");
+        await redisDatabase.StringSetAsync("key4", "Valore 4.1");
+        await redisDatabase.StringSetAsync("key5", "Valore 5.1");
+        await redisDatabase.StringSetAsync("key6", "Valore 6.1");
+
+        return Ok("OK");
+    }
+
+    [Route("read/{key}")]
+    [HttpGet]
+    public async Task<IActionResult> Read(string key)
+    {
+        var db = multiplexer.GetDatabase();
+        var value = await db.StringGetAsync(key);
+
+        return Ok(value.ToString());
+    }
+
+    [Route("pub")]
+    [HttpGet]
+    public async Task<IActionResult> Publish()
+    {
+        var subscriber = multiplexer.GetSubscriber();
+
+        for (int i = 0; i < 20; i++)
         {
-            this.multiplexer = _multiplexer;
-            this.redisDatabase = _redisDatabase;
-            _context = context;
+            await subscriber.PublishAsync("messages", "TEST-----" +  i);
         }
 
-        [Route("db")]
-        [HttpGet]
-        public IActionResult Db()
+        return Ok("OK");
+    }
+
+    /// <summary>
+    /// Esempio di LIST Data Structure in Redis
+    /// </summary>
+    /// <returns></returns>
+    [Route("list")]
+    [HttpGet]
+    public async Task<IActionResult> List()
+    {
+        var db = multiplexer.GetDatabase();
+        var key = "myList";
+
+        //var server = multiplexer.GetServer("127.0.0.1", 6379);
+        // await server.FlushDatabaseAsync(3);  SET AllowAdmin = TRUE
+
+        db.ListLeftPush(key, new RedisValue[]
         {
-            var query =  _context.Travel.Where(i => i.Enabled == true);
-            //var key = query.GetCacheKey();
+            "valore 1",
+            "valore 2",
+            "valore 3",
+            "valore 4",
+            "valore 5",
+            "valore 6"
+        });
 
-            var result = query.FromCache(new TimeSpan(8,0,0));
+        var list = db.ListRange(key, 0, 2);
+        var t = list.ToStringArray();
 
-            return Ok(result);
-        }
-
-        [Route("write")]
-        [HttpGet]
-        public async Task<IActionResult> Write()
-        {
-            await redisDatabase.StringSetAsync("key3", "Valore 3.1");
-            await redisDatabase.StringSetAsync("key4", "Valore 4.1");
-            await redisDatabase.StringSetAsync("key5", "Valore 5.1");
-            await redisDatabase.StringSetAsync("key6", "Valore 6.1");
-
-            return Ok("OK");
-        }
-
-        [Route("read/{key}")]
-        [HttpGet]
-        public async Task<IActionResult> Read(string key)
-        {
-            var db = multiplexer.GetDatabase();
-            var value = await db.StringGetAsync(key);
-
-            return Ok(value.ToString());
-        }
-
-        [Route("pub")]
-        [HttpGet]
-        public async Task<IActionResult> Publish()
-        {
-            var subscriber = multiplexer.GetSubscriber();
-
-            for (int i = 0; i < 20; i++)
-            {
-                await subscriber.PublishAsync("messages", "TEST-----" +  i);
-            }
-
-            return Ok("OK");
-        }
-
-        /// <summary>
-        /// Esempio di LIST Data Structure in Redis
-        /// </summary>
-        /// <returns></returns>
-        [Route("list")]
-        [HttpGet]
-        public async Task<IActionResult> List()
-        {
-            var db = multiplexer.GetDatabase();
-            var key = "myList";
-
-            //var server = multiplexer.GetServer("127.0.0.1", 6379);
-            // await server.FlushDatabaseAsync(3);  SET AllowAdmin = TRUE
-
-            db.ListLeftPush(key, new RedisValue[]
-            {
-                "valore 1",
-                "valore 2",
-                "valore 3",
-                "valore 4",
-                "valore 5",
-                "valore 6"
-            });
-
-            var list = db.ListRange(key, 0, 2);
-            var t = list.ToStringArray();
-
-            return Ok(t);
-        }
+        return Ok(t);
     }
 }
---
+++ Utility/DbUtility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Bogus;
+using RedisStudio.DbContext;
+
+namespace RedisStudio.Utility;
+
+public static class DbUtility
+{
+    public static List<Travel> Feed(int count)
+    {
+        var faker = new Faker<Travel>()
+            .RuleFor(t => t.DossierCode, f => f.Random.AlphaNumeric(10))
+            .RuleFor(t => t.QAIncidentId, f => f.Random.Int(1000, 9999))
+            .RuleFor(t => t.City, f => f.Address.City())
+            .RuleFor(t => t.Nation, f => f.Address.Country())
+            .RuleFor(t => t.Latitudine, f => f.Address.Latitude())
+            .RuleFor(t => t.Longitudine, f => f.Address.Longitude())
+            .RuleFor(t => t.StartDate, f => f.Date.Past())
+            .RuleFor(t => t.EndDate, f => f.Date.Future())
+            .RuleFor(t => t.TransportType, f => f.Random.Int(1, 3))
+            .RuleFor(t => t.InsertDate, f => f.Date.Past())
+            .RuleFor(t => t.InsertBy, f => f.Name.FullName())
+            .RuleFor(t => t.ModifiedDate, f => f.Date.Past())
+            .RuleFor(t => t.ModifiedBy, f => f.Name.FullName())
+            .RuleFor(t => t.Enabled, f => f.Random.Bool());
+
+        return faker.Generate(count);
+    }
+}
+
